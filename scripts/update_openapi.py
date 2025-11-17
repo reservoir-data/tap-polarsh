69d4eef27634d4cfc7e113c2038d63b1818977ce
Edgar Ramírez Mondragón <edgarrm358@gmail.com>
2025-11-16 22:20:43 -0600

    chore: Use stdlib to get the OpenAPI spec (#402)
    Signed-off-by: Edgar Ramírez-Mondragón <edgarrm358@gmail.com>

--- scripts/update_openapi.py
+++ scripts/update_openapi.py
@@ -1,36 +1,39 @@
 #!/usr/bin/env python
-# /// script
-# dependencies = [
-#   # renovate: datasource=pypi depName=requests
-#   "requests==2.32.5",
-# ]
-# ///
 
 """Update the OpenAPI schema from the Polar API.
 
-Copyright (c) 2024 Edgar Ramírez-Mondragón
+Copyright (c) 2025 Edgar Ramírez-Mondragón
 """
 
 from __future__ import annotations
 
+import http
 import json
+import logging
 import pathlib
-
-import requests
+import sys
+import urllib.request
 
 OPENAPI_URL = "https://api.polar.sh/openapi.json"
 PATH = "tap_polarsh/openapi/openapi.json"
 
+logging.basicConfig(format="%(levelname)s - %(message)s", level=logging.INFO)
+logger = logging.getLogger()
+
 
 def main() -> None:
     """Update the OpenAPI schema from the Polar API."""
-    with pathlib.Path(PATH).open("w", encoding="utf-8") as file:
-        response = requests.get(OPENAPI_URL, timeout=5)
-        response.raise_for_status()
-        spec = response.json()
-
+    logger.info("Updating OpenAPI schema from %s", OPENAPI_URL)
+    with (
+        pathlib.Path(PATH).open("w", encoding="utf-8") as f_out,
+        urllib.request.urlopen(OPENAPI_URL) as f_req,  # noqa: S310
+    ):
+        if f_req.status != http.HTTPStatus.OK:
+            logger.error("Failed to fetch OpenAPI spec: %s", f_req.reason)
+            sys.exit()
+        spec = json.load(f_req)
         content = json.dumps(spec, indent=2) + "\n"
-        file.write(content)
+        f_out.write(content)
 
 
 if __name__ == "__main__":
